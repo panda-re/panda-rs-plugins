abbf60e5e6a006690b1d5ccc2838d2cde4aadec9
Andrew Fasano <fasano@mit.edu>
2021-03-19 16:20:31 -0400

    Support running as root without sudo

--- panda-il-trace/setup.sh
+++ panda-il-trace/setup.sh
@@ -1,7 +1,12 @@
 # See: https://github.com/falconre/falcon/blob/master/Dockerfile
 
+SUDO=''
+if (( $EUID != 0 )); then
+    SUDO='sudo'
+fi
+
 # Remove installed capstone version
-sudo apt-get remove -y libcapstone3 libcapstone-dev
+$SUDO apt-get remove -y libcapstone3 libcapstone-dev
 
 # Build from source
 mkdir temp
@@ -10,6 +15,6 @@ wget -nc -q --show-progress https://github.com/aquynh/capstone/archive/4.0.2.tar
 tar xf 4.0.2.tar.gz
 cd capstone-4.0.2
 make -j
-sudo make install
+$SUDO make install
 cd ../../
 rm -rf temp/
